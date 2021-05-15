6f1c73ca2f0bb1e49aa5624cc8af325375d5e5ae
hansol <hansolnoh95@gmail.com>
2021-05-15 15:59:38 +0900

    :tada: 프로젝트 생성

---
+++ Coinone-iOS/Coinone-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coinone-iOS
+//
+//  Created by 노한솔 on 2021/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
