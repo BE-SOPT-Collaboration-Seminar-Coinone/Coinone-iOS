76d948d1d51a1f486054587e6e9372487255de00
Kim-jisoo11 <kingwltn@naver.com>
2021-05-19 17:25:26 +0900

    :lipstick: Main page

---
+++ Coinone-iOS/Coinone-iOS/Source/Models/MyListDataModel.swift
@@ -0,0 +1,21 @@
+//
+//  MyListDataModel.swift
+//  Coinone-iOS
+//
+//  Created by 김지수 on 2021/05/19.
+//
+
+import Foundation
+import UIKit
+
+struct MyListDataModel
+{
+                    var logoImage : String
+                    var companyName : String
+                    var companyNameKR : String
+                    var ratenum : String
+                    var riseRate : String
+                    var RiseImage : String
+                    var compareRiseRate : String
+                    var GraphImage : String
+}
---
+++ Coinone-iOS/Coinone-iOS/Source/ViewControllers/PopupViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PopupViewController.swift
+//  Coinone-iOS
+//
+//  Created by 김지수 on 2021/05/19.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    
+    @IBAction func donotseebuttonclicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closebuttonclicked(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+
+}
